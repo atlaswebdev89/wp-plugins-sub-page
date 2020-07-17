720be8804edde61c75b71f30d946341b00069594
Atlas <doroshuk33@yandex.by>
2020-07-17 10:02:12 +0300

    bugFix

--- wp-atl-sub-page.php
+++ wp-atl-sub-page.php
@@ -7,11 +7,29 @@ Author: Atlas-it
 Author URI: http://atlas-it.by
 Text Domain: atl-sub-page-widget
 Domain Path: /lang/
+License:     GPL2
+
+Copyright 2020  Atlas  (email: atlas.webdev89@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
 */
 
 // строки для перевода заголовков плагина, чтобы они попали в .po файл.
-__( 'Child page by Atlas', 'atl-sub-page-widget' );
-__( 'Creating widget for views child page', 'atl-sub-page-widget' );
+__('Child page by Atlas', 'atl-sub-page-widget' );
+__('Creating widget for views child page', 'atl-sub-page-widget' );
 /*langs file*/
 $plugin_dir = basename( dirname( __FILE__ ) );
 load_plugin_textdomain( 'atl-sub-page-widget', null, $plugin_dir.'/lang/' );
